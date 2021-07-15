57cd7c75a8915385d92111eaada003f925907b13
Christopher Cave-Ayland <c.cave-ayland@imperial.ac.uk>
2021-07-15 17:56:24 +0100

    Add a variety of new features

--- portal_scripts/prep_gaussian.sh
+++ portal_scripts/prep_gaussian.sh
@@ -4,33 +4,41 @@
 com_file=$1
 fchk=$2
 
-chk=$(basename "$fchk" .fchk).chk
+mem_mb=$(echo "$MEMORY_GB * 1024 * 0.9" | bc | xargs printf "%.0f")
+
+top_link0() {
+    # echo only the link0 directives from the top of the input script
+    awk '{if ($1 ~ /^%/) {print $0} else {exit 0}}' "$1"
+}
 
 # strip out any existing resource directives
-sed -i  -e '/%Mem/d' \
-    -e '/%CPU/d' \
-    -e '/%NProcShared/d' \
-    -e '/%GPUCPU/d' \
+sed --in-place \
+    --expression '/%Mem/Id' \
+    --expression '/%CPU/Id' \
+    --expression '/%NProcShared/Id' \
+    --expression '/%GPUCPU/Id' \
     "$com_file"
 
 # replace these 
-sed -i -e "1s/^/%Mem=${MEMORY_GB}GB\n/" \
-    -e "1s/^/%NProcShared=${NCPUS}\n/" \
+sed --in-place \
+    --expression "1s/^/%Mem=${mem_mb}MB\n/" \
+    --expression "1s/^/%NProcShared=${NCORES}\n/" \
     "$com_file"
 
 if [[ -n "${fchk}" ]]
 then
     # chk file uploaded by user so make sure it is used by Gaussian
     # doesn't matter if OldChk and Chk are the same file
-    sed -i -e "1s/^/%OldChk=$chk\n/" "$com_file"
+    chk=$(basename "$fchk" .fchk).chk
+    sed --in-place --expression "1s/^/%OldChk=$chk\n/" "$com_file"
 fi
 
-if ! grep -q "%Chk" "$com_file"
+if ! top_link0 "$com_file" | grep --quiet "%Chk"
 then
     # no Chk directive so set this to a default value
-    sed -i -e "1s/^/%Chk=checkpoint.chk\n/" "$com_file"
+    sed --in-place --expression "1s/^/%Chk=checkpoint.chk\n/" "$com_file"
     echo "checkpoint.chk"
 else
     # print name of specified Chk file
-    grep -oP "%Chk=\K\S+" "$com_file"
+    grep --max-count=1 --only-matching --perl-regexp "%Chk=\K\S+" "$com_file"
 fi
